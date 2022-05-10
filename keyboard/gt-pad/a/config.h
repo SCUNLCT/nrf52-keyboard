3f9cf93a2e8109c83614299ddcb4ad1a1539e9c8
SCUNLCT <105294278+SCUNLCT@users.noreply.github.com>
2022-05-10 21:23:04 +0800

    Update config.h

--- keyboard/gt-pad/a/config.h
+++ keyboard/gt-pad/a/config.h
@@ -26,9 +26,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CONF_VENDOR_ID 0x4366 /* 配置项目内显示的VendorID */
 #define CONF_PRODUCT_ID 0x031A /* 配置项目内显示的ProductID */
 #define DEVICE_VER 0x0000 /* 硬件版本 */
-#define MANUFACTURER "Lotlab" /* 硬件制造商，用于蓝牙显示 */
-#define PRODUCT "GT PAD" /* 硬件名词，用于蓝牙显示 */
-#define MACADDR_SEPRATOR ' ' /* 蓝牙名称后地址的分隔符。若不设置则不显示蓝牙名称后面的地址 */
+#define MANUFACTURER "Lct" /* 硬件制造商，用于蓝牙显示 */
+#define PRODUCT "Lct pad" /* 硬件名词，用于蓝牙显示 */
+//#define MACADDR_SEPRATOR ' ' /* 蓝牙名称后地址的分隔符。若不设置则不显示蓝牙名称后面的地址 */
 
 /* USB HID report parameter */
 #define KEYBOARD_EPSIZE 8 /* 键盘上传端点大小，请不要修改 */
@@ -71,10 +71,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define LED_CAPS 4  //caps led ：5 ； led1、2、3、4：19、20、27、26
 
 // ws2812 RGB 配置
-#define RGB_DI_PIN 1
+#define RGB_DI_PIN 19
 #define RGBLED_NUM 8
 #define RGBLIGHT_ANIMATIONS
-#define RGB_PWR_PIN 0 // P-mos
+#define RGB_PWR_PIN 20 // P-mos
 //#define RGB_PWR_PIN_REVERSE 12 // N-mos
 
 // 3灯指示配置引脚
@@ -88,9 +88,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // RGB 配置
 #define LED_RGB_CC // 是否为共阴LED
-#define LED_RGB_R 5
+#define LED_RGB_R 3
 #define LED_RGB_G 4
-#define LED_RGB_B 3
+#define LED_RGB_B 5
 #define RGB_LIGHT_ANIMATIONS
 
 // 独立硬件按钮
@@ -98,8 +98,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // USB UART 传输配置
 #define HAS_USB // 启用与CH554的通信支持
-#define UART_RXD 6 // UART_RX口IO 17
-#define UART_TXD 7 // UART_TX口IO 18
+#define UART_RXD 7 // UART_RX口IO 17
+#define UART_TXD 6 // UART_TX口IO 18
 //#define UART_DET 19 // UART 检测引脚，若此脚被拉低，则说明USB正在工作。若不配置则使用RX口作为检测引脚
 #define UART_BAUDRATE NRF_UART_BAUDRATE_115200 // 通信波特率，请不要修改
 
@@ -113,9 +113,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // 多用途 Bootloader 按钮
 #define NRF_BL_DFU_MULTI_ROLE_BTN 21
 // Bootloader指示灯
-#define LED_DFU_INIT 5
+#define LED_DFU_INIT 3
 #define LED_DFU_START 4
-#define LED_DFU_FINISH 3
+#define LED_DFU_FINISH 5
 
 // 按键阵列配置
 #define MATRIX_ROWS 5 /* 硬件阵列行数 */
@@ -126,24 +126,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
 
 #if defined(TEST)
-static const uint8_t row_pin_array[MATRIX_ROWS] = {30, 31, 27, 28, 29};
-static const uint8_t column_pin_array[MATRIX_COLS] = {11, 12, 13, 14, 10};
+static const uint8_t row_pin_array[MATRIX_ROWS] = {13, 14, 12, 11, 28};
+static const uint8_t column_pin_array[MATRIX_COLS] = {29, 26, 30, 15, 24};
 #else
-static const uint8_t row_pin_array[MATRIX_ROWS] = {30, 31, 29, 26, 28};
-static const uint8_t column_pin_array[MATRIX_COLS] = {11, 12, 13, 14, 10};
+static const uint8_t row_pin_array[MATRIX_ROWS] = {13, 14, 12, 11, 28};
+static const uint8_t column_pin_array[MATRIX_COLS] = {29, 26, 30, 15, 24};
 #endif
 
 #define ROW_IN // 键盘阵列的二极管方向是从COL->ROW
 
 // 旋钮配置
-#define ROTARY_ENCODER_A 9
-#define ROTARY_ENCODER_B 8
+#define ROTARY_ENCODER_A 23
+#define ROTARY_ENCODER_B 22
 // 旋钮正向按钮映射
 #define ROTARY_ENCODER_POS 4,1
 // 旋钮负向按钮映射
-#define ROTARY_ENCODER_NEG 4,3
+#define ROTARY_ENCODER_NEG 4,2
 // 独立旋钮按键  用于关机后开机
-#define ROTARY_BUTTON 10
+#define ROTARY_BUTTON 24
 // SSD1306 LCD屏幕
 #define SSD1306_SDA 25
 #define SSD1306_SCL 27
@@ -154,13 +154,6 @@ static const uint8_t column_pin_array[MATRIX_COLS] = {11, 12, 13, 14, 10};
 #define DEBOUNCE 5 /* 硬件消抖次数，设置为0则不消抖 */
 #define MATRIX_SCAN_DELAY_CYCLE 48 /* 按键扫描等待IO稳定的延时时长 */
 
-// 采用内部RC
-#define NRFX_CLOCK_CONFIG_LF_SRC 0
-#define CLOCK_CONFIG_LF_SRC 0
-#define NRF_SDH_CLOCK_LF_SRC 0
-#define NRF_SDH_CLOCK_LF_RC_CTIV 16
-#define NRF_SDH_CLOCK_LF_RC_TEMP_CTIV 2
-#define NRF_SDH_CLOCK_LF_ACCURACY 1
 
 //配置RTC预分频器.
 // <0> 32MHz <1> 16MHz <3> 8MHz <7> 4MHz <15> 2MHz <31> 1MHz 
